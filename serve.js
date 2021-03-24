c66e35ffa97e65a4a732aa43e024ce538d5c38f2
jponharn <jukkrapong.ponharn@nectec.or.th>
2021-03-24 15:27:02 +0700

    deploy

--- serve.js
+++ serve.js
@@ -3,10 +3,10 @@ var session = require('express-session')
 let setting = require('./setting.json')
 const fetch = require('node-fetch')
 const fs = require('fs')
-// var app = express()
+var app = express()
 
-const app = require("https-localhost")()
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+// const app = require("https-localhost")()
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 
 const oxd = require('oxd-node')(setting.oxd_setting);
 
